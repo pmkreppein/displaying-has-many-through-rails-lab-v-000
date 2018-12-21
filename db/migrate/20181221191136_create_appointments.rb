7ec0173b2fd1bdd1ede2e572790d5142f9988334
Peter Kreppein <pmkreppein@gmail.com>
2018-12-21 19:12:03 +0000

    Automatically backed up by Learn

--- db/migrate/20181221191136_create_appointments.rb
+++ db/migrate/20181221191136_create_appointments.rb
@@ -1,7 +1,11 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-
+      t.datetime "appointment_datetime"
+      t.integer  "doctor_id"
+      t.integer  "patient_id"
+      t.datetime "created_at",           null: false
+      t.datetime "updated_at",           null: false
       t.timestamps null: false
     end
   end
